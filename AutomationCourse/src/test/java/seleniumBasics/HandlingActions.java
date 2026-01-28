1dfe0ed62672b4f705164b904a0282d2d3aaf208
HOME <HOME@DESKTOP-993FB2V>
2026-01-28 22:03:57 +0530

    Fifth commit

---
+++ AutomationCourse/src/test/java/seleniumBasics/HandlingActions.java
@@ -0,0 +1,42 @@
+package seleniumBasics;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+
+public class HandlingActions extends Base{
+	
+	public void verifyRightClick() {
+		WebElement other=driver.findElement(By.id("others"));
+		Actions a=new Actions(driver);
+		a.contextClick(other).build().perform();	
+	}
+	
+	public void verifyMouseOver() {
+		WebElement other=driver.findElement(By.id("others"));
+		Actions a=new Actions(driver);
+		a.moveToElement(other).build().perform();
+	}
+	
+	public void verifyDragAndDrop() {
+		driver.navigate().to("https://demoqa.com/droppable");
+		
+		WebElement drag=driver.findElement(By.id("draggable"));
+		WebElement drop=driver.findElement(By.id("droppable"));
+		Actions a=new Actions(driver);
+		a.dragAndDrop(drag, drop).build().perform();
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HandlingActions action=new HandlingActions();
+		action.browserLaunch();
+		//action.verifyRightClick();
+		//action.verifyMouseOver();
+		action.verifyDragAndDrop();
+	}
+	
+	
+
+}
